a6481487e52e9fbc224a98e564102530b0443771
Abahor <abahormelad@gmail.com>
2023-02-17 06:35:18 -0800

    as

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry program 4
+* Return: 0 (Success)
+*/
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+	if (c != 'q' && c != 'e')
+		putchar(c);
+	c++;
+}
+putchar('\n');
+return (0);
+}
