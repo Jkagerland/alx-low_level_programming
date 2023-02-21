d9d0a25b3d0417b51320ae164c6011955363218f
Abahor <abahormelad@gmail.com>
2023-02-21 08:23:25 -0800

    as

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Entry to the program
+* Return: 0 (success)
+*/
+
+void print_alphabet_x10(void)
+{
+
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	}
+
+}
