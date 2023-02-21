d52100bc5bd93449e1d2f78b169a61d39842beee
Abahor <abahormelad@gmail.com>
2023-02-21 11:11:15 -0800

    da

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Prints the sum
+* Return: Always succes
+*/
+
+int main(void)
+{
+	int i = 0;
+	int sum = 0;
+	while (i < 1024)
+	{
+	sum += i;
+	i = i + 3;
+	}
+	i = 0;
+	while (i < 1024)
+	{
+	sum += i;
+	i = i + 5;
+	}
+	printf("%d\n", sum);
+}
