76960922709e6b1cf7c81459adbdda2f70af9e7d
Abahor <abahormelad@gmail.com>
2023-02-21 08:07:23 -0800

    das

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - check the code
+* Return: 0 (success)
+*/
+
+void print_alphabet(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
