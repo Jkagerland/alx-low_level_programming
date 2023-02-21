82cc87e9d4b91f14ade0b45ccb8c70ff703d982f
Abahor <abahormelad@gmail.com>
2023-02-21 11:59:33 -0800

    das

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,23 +8,20 @@
 int main(void)
 {
 	int i;
-	int temp;
-	long last_num = 2;
-	long before_last = 1;
+	long int temp;
+	long int last_num = 2;
+	long int before_last = 1;
 
-	for(i = 0; i < 50; i++)
+	for(i = 0; i < 49; i++)
 	{
 	if (i == 0)
-	printf("%ld, %ld, ", before_last, last_num);
+	printf("%ld, %ld", before_last, last_num);
 	else
 	{
 	temp = last_num;
 	last_num = before_last + last_num;
 	before_last = temp;
-	if (i != 49)
-	printf("%ld, ", last_num);
-	else
-	printf("%ld", last_num);
+	printf(", %ld", last_num);
 	}
 	}
 	printf("\n");
