03cdb26fac43d87cf6cbf28ed089e8d3db04d60a
Abahor <abahormelad@gmail.com>
2023-02-21 07:43:29 -0800

    dasD

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,17 @@
 #include "main.h"
-
 /**
 * main - Entry to the program
 * Return: 0 (Succes)
 */
 int main(void){
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
