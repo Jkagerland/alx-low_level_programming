def507952bd3d539f2fc41769a050a14dde13381
Abahor <abahormelad@gmail.com>
2023-02-23 03:11:28 -0800

    Ad

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_square - f
+* @size: t
+* Return: 0
+*/
+
+void print_square(int size)
+{
+	int i;
+	int r;
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	for(i = 0; i < size; i++)
+	{
+	for (r = 0; r < size; r++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+}
