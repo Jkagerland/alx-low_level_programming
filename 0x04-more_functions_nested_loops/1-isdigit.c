cd306efd4c7cca8168b80c7a8502295f541d9a9c
Abahor <abahormelad@gmail.com>
2023-02-23 02:12:00 -0800

    dsa

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isdigit - Entry point
+* @c: the  num
+* Return: 0 success
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
