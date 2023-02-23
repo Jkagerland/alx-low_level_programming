9b255d3d85b1f178943b8f32dbc790d5e7383702
Abahor <abahormelad@gmail.com>
2023-02-23 02:20:49 -0800

    ads

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - Entry
+* Return: 0 s
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+
+}
