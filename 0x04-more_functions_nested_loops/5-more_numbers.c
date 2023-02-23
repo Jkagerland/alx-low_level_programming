e2c94581d00956b5a5a7107e52f380fff226a072
Abahor <abahormelad@gmail.com>
2023-02-23 02:35:13 -0800

    asd

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - Entry
+* Return: 0 success
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int r;
+
+	for (i = 0; i < 10; i++ )
+	{
+	for (r = 0; r < 14; r++)
+	{
+	_putchar(r + '0');
+	}
+	_putchar('\n');
+	}
+	_putchar('\n');
+}
