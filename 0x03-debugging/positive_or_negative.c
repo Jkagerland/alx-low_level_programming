c3494eccae310cd8a747667bfbcbe1eb7c6d405a
Abahor <abahormelad@gmail.com>
2023-02-23 16:49:13 -0800

    Ad

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -4,21 +4,19 @@
 /* more header goes there */
 
 /**
-* main - Entry to the program
+* positive_or_negative - Entry to the program
+* @i: t
 * Return: 0 (success)
 */
 
-int main(void)
+int positive_or_negative(int i)
 {
-        int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if (n > 0)
-                printf("%d is positive\n", n);
-        else if (n == 0)
-                printf("%d is zero\n", n);
+        if (i > 0)
+                printf("%d is positive\n", i);
+        else if (i == 0)
+                printf("%d is zero\n", i);
         else
-                printf("%d is negative\n", n);
+                printf("%d is negative\n", i);
         return (0);
 }
