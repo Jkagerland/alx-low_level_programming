76aa7a933a6494f3b30368b4b52d80c378935615
Abahor <abahormelad@gmail.com>
2023-03-06 16:16:52 -0800

    asd

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _memset - entry point
+* @s: t
+* @b: t
+* @n: t
+* Return: 0
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+	s[i] = b;
+	n--;
+	}
+	return (s);
+}
