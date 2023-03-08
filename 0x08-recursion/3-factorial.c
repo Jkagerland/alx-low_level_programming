77201ced90655647699b1615afadf9bf246cd62a
Abahor <abahormelad@gmail.com>
2023-03-08 07:19:13 -0800

    ad

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,12 +9,12 @@
 int factorial(int n)
 {
 
-	if (n < 0)
-		return (-1);
-        if (n > 1)
-        {
-        n = n * factorial(n - 1);
-        }
+if (n < 0)
+	return (-1);
+if (n > 1)
+{
+n = n *factorial(n - 1);
+}
 
-        return (n);
+return (n);
 }
