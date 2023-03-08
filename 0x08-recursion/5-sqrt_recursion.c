8fcf90c5a322affaec1de1c0f4a33ac39523b64c
Abahor <abahormelad@gmail.com>
2023-03-08 08:14:57 -0800

    :dsa

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,42 @@
-int find_root(int n, int r){
+/**
+* find_root - En
+* @n: nu
+* @r: root
+* Return: 0
+*/
 
-    if ((r * r) < n )
-        return find_root(n, r+1);
-    else
-        return(r);
+
+int find_root(int n, int r)
+{
+
+if ((r * r) < n)
+{
+return (find_root(n, r + 1));
 }
+else
+{
+return (r);
+}
+}
+
+/**
+* _sqrt_recursion - Entry
+* @n: num
+* Return: 0
+*/
 
 int _sqrt_recursion(int n)
 {
-    int x;
-    if (n < 0)
-    {
-        return n;
-   }
-    x = find_root(n, 0);
+int x;
+if (n < 0)
+{
+return (-1);
+}
+x = find_root(n, 0);
 
-    if ((x * x) != n){
-        return (-1);
-    }
-    return (x);
+if ((x *x) != n)
+{
+return (-1);
+}
+return (x);
 }
