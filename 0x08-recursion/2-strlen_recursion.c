481229a07766caadfd55e089c00f3d52634f6f90
Abahor <abahormelad@gmail.com>
2023-03-08 06:46:47 -0800

    g

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,11 +10,11 @@ int _strlen_recursion(char *s)
 int i = 0;
 
 if (*s)
-    {
-        i++;
-        i += _strlen_recursion(s+1);
-    }
+{
+i++;
+i += _strlen_recursion(s + 1);
+}
 
-    return i;
+return (i);
 }
 
