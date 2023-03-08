ab06c3ca0f95dcaa64d37570a884cb65b4ba60ae
Abahor <abahormelad@gmail.com>
2023-03-08 07:25:47 -0800

    :g::

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _pow_recursion - Entry
+* @x: t
+* @y: t
+* Return: the nums
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y >= 2)
+	{
+	x = x * _pow_recursion(x , y - 1);
+	}
+	return (x);
+}
