6bd7897ab04ef22d1b8b2c00405d875db1d9f12f
Abahor <abahormelad@gmail.com>
2023-03-08 06:06:02 -0800

    g

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
-/*
-* _puts_recursion - Entry point
-* @s: t
-* Return: 0
-*/
+/**
+ * _puts_recursion - function like puts()
+ * @s: input
+ * Return: 0 (success)
+ */
 
 void _puts_recursion(char *s)
 {
 
 	if (*s)
 	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
