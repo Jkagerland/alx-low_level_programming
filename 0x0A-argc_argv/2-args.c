95651cccaa0a827abe39cf66aa4b8b7719c72a14
Abahor <abahormelad@gmail.com>
2023-03-10 09:02:26 -0800

    g

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - E
+* @argc: num
+* @argv: arr
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for(; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
