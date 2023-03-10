a2ddb4852f1d0fe7b6e240cfe9097dc6b9ae27a1
Abahor <abahormelad@gmail.com>
2023-03-10 08:55:40 -0800

    jnjn

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - E
+* @argc: c
+* @argv: v
+* Return: 0
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
