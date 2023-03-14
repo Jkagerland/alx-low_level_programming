0dac85ed3ab5fc3c4de0a0ccc2c2ac43521fb586
Abahor <abahormelad@gmail.com>
2023-03-13 18:21:31 -0700

    g

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* create_array - Entry point
+* @size: i
+* @c: char
+* Return: 0
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i = 0;
+
+	if (size == 0)
+	{
+	return (NULL);
+	}
+	s = malloc(sizeof(char) * (size + 1));
+	while (i < size)
+	{
+	s[i] = c;
+	i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
