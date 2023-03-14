5f72add37a091d52b7f9c5e25e0dbb6cdcb14bc9
Abahor <abahormelad@gmail.com>
2023-03-13 19:58:28 -0700

    dsa

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,28 +9,26 @@
 int **alloc_grid(int width, int height)
 {
 	int **t;
-	int i, o;
-
-	i = 0;
-	o = 0;
 	if (width <= 0 || height <= 0)
-		return (NULL);
-	t = malloc(sizeof(int) * height);
+	{
+	free(t);
+	return (NULL);
+	}
+	t = malloc(sizeof(int) * (height + 1));
 	if (t == NULL)
 	{
 	free(t);
 	return (NULL);
 	}
-	for (; i < height; i++)
+	for (int i = 0; i < height; i++)
 	{
-	t[i] = malloc(sizeof(int) * width);
+	t[i] = malloc(sizeof(int) * (width + 1));
 	if (t[i] == NULL)
 	{
 	free(t);
 	return (NULL);
 	}
-	o = 0;
-	for (; o < width; o++)
+	for (int o = 0; o < width; o++)
 	{
 	t[i][o] = 0;
 	}
