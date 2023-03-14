c6973c3d9399a66f781f3a905e9d200f81886ffa
Abahor <abahormelad@gmail.com>
2023-03-13 18:28:34 -0700

    das

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,17 +11,15 @@ char *create_array(unsigned int size, char c)
 {
 	char *s;
 	unsigned int i = 0;
-	s = malloc(sizeof(char) * (size + 1));
-	if (s == NULL)
+	s = malloc(sizeof(char) * (size));
+	if (s == NULL || size == 0)
 	{
 	return (NULL);
 	}
-	/* s = malloc(sizeof(char) * (size + 1));*/
 	while (i < size)
 	{
 	s[i] = c;
 	i++;
 	}
-	s[i] = '\0';
 	return (s);
 }
