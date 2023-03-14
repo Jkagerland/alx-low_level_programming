b8696fe67cb76a68c1cc7394aab692ccb0d23c37
Abahor <abahormelad@gmail.com>
2023-03-13 18:49:25 -0700

    aD

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _strdup - Entry
+* @str: s
+* Return: 0
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *s;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	while (str[i] != '\0')
+		i++;
+
+	s = malloc(sizeof(char) * i);
+
+	if (s == NULL)
+	{
+	return (NULL);
+	}
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+	s[i] = str[i];
+	i++;
+	}
+	s[i] = '\0';
+
+	return (s);
+}
